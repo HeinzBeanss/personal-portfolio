d51de14a422e446e699bfefd909e66888657ef2c
David Bean <heinzbeansss@gmail.com>
2023-01-29 06:05:01 +0000

    Added typing animation to title

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -7,18 +7,18 @@ const Contact = () => {
         <div className="contact" >
                 <h2 className="titlecontact">Get in touch!</h2>
                 <p className="desccontact">Have any questions? I'd love nothing more than to answer them, feel free to drop me an email using the form below!</p>
-                <form class="contactform" action="https://formsubmit.co/f9c5245a9e75583fb8ccc82f0885ca47" method="POST">
+                <form className="contactform" action="https://formsubmit.co/f9c5245a9e75583fb8ccc82f0885ca47" method="POST">
 
-                    <label for="name">NAME</label>
+                    <label htmlFor="name">NAME</label>
                     <input id="name" type="text" name="name" placeholder="Jack Sparrow" required></input>
                     
-                    <label for="email">EMAIL ADDRESS</label>
+                    <label htmlFor="email">EMAIL ADDRESS</label>
                     <input id="email" type="email" name="email" placeholder="whyistherumgone@gmail.com" required></input>
                     
-                    <label for="subject">SUBJECT</label>
+                    <label htmlFor="subject">SUBJECT</label>
                     <input id="subject" name="subject" type="text" placeholder="The Black Pearl"></input>
 
-                    <label for="message">MESSAGE</label>
+                    <label htmlFor="message">MESSAGE</label>
                     <textarea id="message" type="text" name="message" cols="40" rows="5" placeholder="Write your message here..." required></textarea>
                     
                     <button className="submitbutton" type="submit">Send Message</button>
--- src/Components/Introduction.js
+++ src/Components/Introduction.js
@@ -1,13 +1,73 @@
 import React from "react";
 import "../CSS/Introduction.css"
 
+import Typewriter from 'typewriter-effect';
+
 const Introduction = () => {
 
     return (
         <div className="introduction">
             <div className="introductiontitle">
-                <h1 className="introtitlehello">Hello!</h1>
-                <h1 className="introtitlemynameis">I'm David.</h1>
+                <h1 className="introtitlehello">
+                    <Typewriter options={{
+                      autoStart: true,
+                      loop: false,
+                      delay: 80,
+                    }}
+                      onInit={(typewriter) => {
+
+                        typewriter.typeString('<div class="abc123">Hello!</div>')
+                        .pauseFor(1200)
+                        .callFunction(() => {
+                            const cursorType = document.querySelector(".Typewriter__cursor");
+                            cursorType.style = "visibility:hidden";
+                        })
+                          .start();
+                      }}
+                    />
+                </h1>
+
+                <h1 className="introtitlemynameis">
+                    <Typewriter
+                    options={{
+                        loop: true,
+                        delay: 80,
+                        deleteSpeed: 0,
+                      }}
+
+                    onInit={(typewriter) => {
+                    typewriter
+                      .callFunction(() => {
+                          const cursorTypeOne = document.querySelectorAll(".Typewriter__cursor")[1];
+                          console.log(cursorTypeOne);
+                          cursorTypeOne.style = "visibility:hidden";
+                      })
+                      .pauseFor(2000)
+                      .callFunction(() => {
+                        const cursorTypeOne = document.querySelectorAll(".Typewriter__cursor")[1];
+                        cursorTypeOne.style = "visibility:visible";
+                    })
+                    .pauseFor(200)
+                      .typeString('<div class="abc123">I\'m a programming enthusiast!</div>')
+                      .pauseFor(1200)
+                      .deleteChars(25)
+                      .pauseFor(500)
+                      .typeString("an aspiring developer.")
+                      .pauseFor(1200)
+                      .deleteChars(22)
+                      .pauseFor(500)
+                      .typeString("your next employee?")
+                      .pauseFor(1200)
+                      .deleteChars(19)
+                      .pauseFor(500)
+                      .typeString('<strong><h1 style="color: var(--primary)">David</h1></strong>')
+                      .pauseFor(1500)
+                      .typeString(" :)")
+                      .pauseFor(10000)
+                      .start();
+                  }}
+                />
+            </h1>
                 {/* <h1 className="introtitledavid"></h1> */}
             </div>
 
